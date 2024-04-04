c752dbd2b4bb804d9320c8620c24b3323dd77a37
Luis <u202013691@upc.edu.pe>
2024-04-04 08:53:02 -0500

    Prueba

---
+++ src/main/java/pe/edu/upc/demogrupo3_sv64/DemoGrupo3Sv64Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demogrupo3_sv64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrupo3Sv64Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGrupo3Sv64Application.class, args);
+    }
+
+}
