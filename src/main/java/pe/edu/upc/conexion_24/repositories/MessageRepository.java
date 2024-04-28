2e1972821e145277b248612e208784cc91a9d05b
Jhon Huaman <81259628+jhonhuaman@users.noreply.github.com>
2024-04-27 22:51:34 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/conexion_24/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.conexion_24.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.conexion_24.entities.Message;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
