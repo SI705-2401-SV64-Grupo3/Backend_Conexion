05c14a7f3fa92243e950a10ef715e6f764dff239
Jhon Huaman <81259628+jhonhuaman@users.noreply.github.com>
2024-04-27 21:55:11 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/conexion_24/repositories/PublicationRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.conexion_24.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.conexion_24.entities.Publication;
-
-@Repository
-public interface PublicationRepository extends JpaRepository<Publication, Long> {
-}
--- src/main/java/pe/edu/upc/conexion_24/repositories/SuggestionsRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.conexion_24.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.conexion_24.entities.Suggestions;
-
-public interface SuggestionsRepository extends JpaRepository<Suggestions, Long> {
-}
